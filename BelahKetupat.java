8e512c4183f89e42700b11cc6679ce1d9d2f489a
Agni Octavia <agnioctavia2@gmail.com>
2017-04-06 09:44:55 +0700

    Update BelahKetupat.java

--- BelahKetupat.java
+++ BelahKetupat.java
@@ -17,33 +17,33 @@ public class BelahKetupat {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Agni Octavia (6706150133)");
-System.out.println("");
+System.out.println("0");
 int l = 1;
 int tingkat = 10;
 int n = 12/2;
 for(int i=1;i<=n;i++){
 for(int k=n;k>i;k--){
-System.out.print(" ");
+System.out.print("0");
 }
 for(int j=1;j<=l;j++){
 System.out.print("+");
 }
 l+=2;
  
-System.out.println("");
+System.out.println("0");
 }
 l -= 4;
  
 for(int i=(n-1);i>=1;i--){
 for(int k=i;k<=(n-1);k++){
-System.out.print(" ");
+System.out.print("0");
 }
 for(int j=l;j>=1;j--){
 System.out.print("+");
 }
 l-=2;
  
-System.out.println("");
+System.out.println("0");
 }
     }
     
